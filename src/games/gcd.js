89eb79f4070a4d8be0d023daa5f76e67d5e01550
Tariel <uzumake02@mail.ru>
2019-08-29 11:17:01 +0400

    Refactoring, update to 1.6.17

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,7 +3,7 @@ import getRandomInt from '../utils';
 
 const ruleOfGame = 'Find the greatest common divisor of given numbers.';
 
-const getGcd = (num1, num2) => num2 === 0 ? num1 : getGcd(num2, num1 % num2);
+const getGcd = (num1, num2) => (num2 === 0 ? num1 : getGcd(num2, num1 % num2));
 
 const gameGcd = () => {
   const num1 = getRandomInt(1, 50);
